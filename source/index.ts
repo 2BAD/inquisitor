8ead88afcd3d7c8770e2ecc400e8616373c681ff
Jason Hyde <github@2bad.me>
2021-04-06 16:16:21 +0300

    [ts] do not declare return type explicitly, for now

--- source/index.ts
+++ source/index.ts
@@ -1,8 +1,7 @@
-import type { PlainResponse } from 'got'
 import { client } from '~/client'
 
 export const Inquisitor = () => {
   return {
-    time: (url: string): Promise<PlainResponse['timings']> => client.get(url).then(r => r.timings)
+    time: (url: string) => client.get(url).then(r => r.timings),
   }
 }
