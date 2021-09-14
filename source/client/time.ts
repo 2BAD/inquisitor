b72e7fa4304399a49bf3dd9f961a8bc79acd42cd
Jason Hyde <github@2bad.me>
2021-09-14 22:11:27 +0300

    [ts] explicitly set return type for times function

--- source/client/time.ts
+++ source/client/time.ts
@@ -1,3 +1,4 @@
+import { Timings } from './../types/timings'
 import { client } from './index'
 
-export const time = (url: string) => client.get(url).then((r) => r.timings)
+export const time = (url: string): Promise<Timings> => client.get(url).then((r) => r.timings)
