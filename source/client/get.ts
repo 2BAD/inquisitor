1c807174c9a3d62f31e5ca7bec1f35c326cbb918
Jason Hyde <github@2bad.me>
2021-11-01 19:02:34 +0200

    [ts] add getter for headers

--- source/client/get.ts
+++ source/client/get.ts
@@ -1,5 +1,7 @@
-import { Timings } from '../types/timings'
+import type { Headers } from 'got'
+import type { Timings } from '../types/timings'
 import { client } from './index'
 
-export const content = (url: string): Promise<string> => client.get(url).then((r) => r.body)
-export const timings = (url: string): Promise<Timings> => client.get(url).then((r) => r.timings)
+export const content = (url: string) => (): Promise<string> => client.get(url).then((r) => r.body)
+export const timings = (url: string) => (): Promise<Timings> => client.get(url).then((r) => r.timings)
+export const headers = (url: string) => (): Promise<Headers> => client.head(url).then((r) => r.headers)
