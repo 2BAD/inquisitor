fc1c1f9dc4eb5cdf6b3ba932e488c9c610cc2bf3
Jason Hyde <github@2bad.me>
2021-11-03 17:54:58 +0200

    [ts] configurable resolver and url for main instance

--- source/index.ts
+++ source/index.ts
@@ -1,11 +1,22 @@
 import type { Resolver } from 'dns/promises'
 import type { Inquire } from './types/inquire'
+import { timings, content } from './client/get'
+import { createResolver, setupResolve } from './dns/resolve'
+import { URL } from 'url'
 
-export const Inquisitor = () => {
+export const Inquisitor = (url: string, resolver?: Resolver): Inquire => {
+  const { hostname } = new URL(url)
+  resolver = resolver ?? createResolver()
+  const resolve = setupResolve(resolver)(hostname)
+
   return {
-    time,
+    timings: timings(url),
+    content: content(url),
     dns: {
-      min
+      a: resolve('A'),
+      cname: resolve('CNAME'),
+      mx: resolve('MX'),
+      txt: resolve('TXT')
     }
   }
 }
